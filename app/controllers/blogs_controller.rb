2982a57a991efd2f68e8064bb723c950856bbf1f
Dexter Goh <bboifatt@gmail.com>
2020-04-13 02:31:34 +0800

    added in home action

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,8 +1,13 @@
 class BlogsController < ApplicationController
+  def home
+  end
+
   def index
   end
 end
 
+# i can start by designing the day 1 and try to integrate javascript into this
+# i should look into wiki feature
 
 
 # i envision a gallery of responsive cards showcasing day 1 to day 100
